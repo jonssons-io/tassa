10a4498dda0f5937fc2424bcf760f2a22f0e8751
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-01-21 14:00:58 +0100

    Fixes MatchingBanner

--- src/components/MatchingPageComponents/MatchingBanner.vue
+++ src/components/MatchingPageComponents/MatchingBanner.vue
@@ -1,70 +1,75 @@
 <template>
-  <b-container class="bv-example-row">
-    <b-col>
-      <h1 class="match-header">Matchningar</h1>
-    </b-col>
-    <b-row>
-      <b-col>
-        <lable class="match-label">Namn</lable>
-      </b-col>
-      <b-col>
-        <lable class="match-label">Område</lable>
-      </b-col>
-      <b-col>
-        <lable class="match-label">Familj</lable>
-      </b-col>
-      <div class="w-100"></div>
-      <b-col class="B-C-A"><h6 class="match-list">A.Svensson</h6></b-col>
-      <b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C-A"
-        ><h6 class="match-list">Boppe, 6 år, Basset-Hane</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C"><h6 class="match-list">P.Josefsson</h6></b-col>
-      <b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C"
-        ><h6 class="match-list">Tilda, 3 år, Greyhound-Tik</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C-A"><h6 class="match-list">J.Andersson</h6></b-col>
-      <b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C-A"
-        ><h6 class="match-list">Snowy, 6 år, Chihuahua-Tik</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C"><h6 class="match-list">R.Larsson</h6></b-col>
-      <b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C"
-        ><h6 class="match-list">Happy, 2 år, Gand Danois-Hane</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C-A"><h6 class="match-list">P.Karlsson</h6></b-col>
-      <b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C-A"
-        ><h6 class="match-list">Kuma, 3 år, Podengo-Hane</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C"><h6 class="match-list">C.Ohlsson</h6></b-col>
-      <b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C"
-        ><h6 class="match-list">Bullen, 8 år, Norwhich Terrier-Hane</h6></b-col
-      >
-      <div class="w-100"></div>
-      <b-col class="B-C-A"><h6 class="match-list">H.Persson</h6></b-col>
-      <b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
-      <b-col class="B-C-A"
-        ><h6 class="match-list">Lilly, 4 år, Golden Retriver-Tik</h6></b-col
-      >
-    </b-row>
-  </b-container>
+	<b-container class="bv-example-row">
+		<b-col>
+			<h1 class="match-header">Matchningar</h1>
+		</b-col>
+		<b-row>
+			<b-col>
+				<lable class="match-label">Namn</lable>
+			</b-col>
+			<b-col>
+				<lable class="match-label">Område</lable>
+			</b-col>
+			<b-col>
+				<lable class="match-label">Familj</lable>
+			</b-col>
+			<div class="w-100"></div>
+			<b-col class="B-C-A"><h6 class="match-list">A.Svensson</h6></b-col>
+			<b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C-A"
+				><h6 class="match-list">Boppe, 6 år, Basset-Hane</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C"><h6 class="match-list">P.Josefsson</h6></b-col>
+			<b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C"
+				><h6 class="match-list">Tilda, 3 år, Greyhound-Tik</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C-A"><h6 class="match-list">J.Andersson</h6></b-col>
+			<b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C-A"
+				><h6 class="match-list">Snowy, 6 år, Chihuahua-Tik</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C"><h6 class="match-list">R.Larsson</h6></b-col>
+			<b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C"
+				><h6 class="match-list">
+					Happy, 2 år, Gand Danois-Hane
+				</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C-A"><h6 class="match-list">P.Karlsson</h6></b-col>
+			<b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C-A"
+				><h6 class="match-list">Kuma, 3 år, Podengo-Hane</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C"><h6 class="match-list">C.Ohlsson</h6></b-col>
+			<b-col class="B-C"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C"
+				><h6 class="match-list">
+					Bullen, 8 år, Norwhich Terrier-Hane
+				</h6></b-col
+			>
+			<div class="w-100"></div>
+			<b-col class="B-C-A"><h6 class="match-list">H.Persson</h6></b-col>
+			<b-col class="B-C-A"><h6 class="match-list">Ö.Svealand</h6></b-col>
+			<b-col class="B-C-A"
+				><h6 class="match-list">
+					Lilly, 4 år, Golden Retriver-Hane
+				</h6></b-col
+			>
+		</b-row>
+	</b-container>
 </template>
 
 <script>
 export default {
-  name: "Match"
+	name: "Match"
 };
 import "./../../assets/css/matchingPage.css";
 </script>
 
-<style scoped lang="sass">
-</style>
+<style scoped lang="sass"></style>
