7096fd67a559c218d7d27c6e9729d114925ff22e
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-01-21 13:05:34 +0100

    Removes buttons from view

--- src/views/RegisterUser.vue
+++ src/views/RegisterUser.vue
@@ -1,14 +1,12 @@
 <template>
 	<b-container fluid class="mh-100 mw-100">
 		<RegisterTitle title="Registrera dig" />
-		<RegisterUserForm />
-		<RegisterButtons ctabtntext="Nästa" />
+		<RegisterUserForm ctabtntext="Nästa" />
 		<NavBar />
 	</b-container>
 </template>
 
 <script>
-import RegisterButtons from "@/components/RegisterComponents/RegisterButtons";
 import RegisterUserForm from "@/components/RegisterComponents/RegisterUserForm";
 import RegisterTitle from "@/components/RegisterComponents/RegisterTitle";
 import NavBar from "@/components/NavBar";
@@ -16,7 +14,6 @@ import NavBar from "@/components/NavBar";
 export default {
 	name: "RegisterUser",
 	components: {
-		RegisterButtons,
 		RegisterUserForm,
 		RegisterTitle,
 		NavBar
