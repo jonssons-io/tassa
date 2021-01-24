dde597981e5a2235882c2898848b8b26707fa64d
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-01-25 00:04:18 +0100

    Added dogtemplate to store Added description template to store

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,27 @@ export default new Vuex.Store({
 			phone: "",
 			gender: "",
 			area: "Huddinge",
-			profilepicture: require("@/assets/DefaultPicture.svg")
+			desc:
+				"Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+			profilepicture: require("@/assets/DefaultPicture.svg"),
+			dogs: [
+				{
+					name: "maja",
+					gender: "tik",
+					age: "8",
+					size: "small",
+					breed: "Jack Russel",
+					dogpicture: require("@/assets/DefaultDog.svg")
+				},
+				{
+					name: "nisse",
+					gender: "hane",
+					age: "3",
+					size: "medium",
+					breed: "Drever",
+					dogpicture: require("@/assets/DefaultDog.svg")
+				}
+			]
 		},
 		userRegistration: {
 			firstname: "",
