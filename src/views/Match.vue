9b63ba9a7536d4539dc44ce14b0d7616827ca846
Mikaela Fryklund <mikfry@Mikaelas-MacBook.local>
2021-01-28 09:12:02 +0100

    new things added

--- src/views/Match.vue
+++ src/views/Match.vue
@@ -1,27 +1,27 @@
 <template>
-	<div class="matching--container">
-		<MatchingBanner />
-		<NavBar />
-	</div>
+  <div class="matching--container">
+    <MatchingBanner />
+    <NavBar />
+  </div>
 </template>
 
 <script>
 import MatchingBanner from "@/components/MatchingPageComponents/MatchingBanner.vue";
+//import MatchingBanner from "@/components/MatchingPageComponents/MatchingBanner.vue";
 import NavBar from "@/components/NavBar.vue";
 
 export default {
-	name: "Match",
-	components: {
-		MatchingBanner,
-		NavBar
-	}
+  name: "Match",
+  components: {
+    MatchingBanner,
+    NavBar
+  }
 };
 </script>
 
 <style >
 .matching--container {
-    background-color:  #005b52;
-    height: 100%;
+  background-color: #005b52;
+  height: 100%;
 }
-
 </style>
