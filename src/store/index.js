72e058fa17fa3b01633a47af20900c9a399400f1
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-02-16 16:03:55 +0100

    Removed unused state

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,19 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import ApiHandler from "@/util/ApiHandler.js";
 import CookieHandler from "../util/CookieHandler";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		dogRegistration: {
-			name: "",
-			gender: "",
-			age: "",
-			size: "",
-			breed: ""
-		},
 		navbarState: CookieHandler.getCookie("authstatus") === "true"
 	},
 	mutations: {
-		saveDogForm(state, payload) {
-			state.dogRegistration = payload.registerdogform;
-		},
 		updateNavbar(state, value) {
 			state.navbarState = value;
 		}
 	},
 	actions: {
-		GET_USER({ commit }, userid) {
-			return new Promise((resolve, reject) => {
-				return ApiHandler.getUser(userid)
-					.then(res => {
-						commit("updateUserInfo", res);
-						resolve(res);
-					})
-					.catch(error => {
-						reject(error);
-					});
-			});
-		},
 		SET_NAVBAR({ commit }) {
 			return new Promise(resolve => {
 				if (CookieHandler.getCookie("authstatus")) {
