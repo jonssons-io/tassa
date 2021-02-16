571f89ffa008f8238583f8e276a55ca9fa828f2a
Gabrielle <dev.gabrielle.jonsson@gmail.com>
2021-02-16 17:46:48 +0100

    Fixes issue where components did not update on route param change

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -26,25 +26,20 @@ export default {
 		ProfileFamily,
 		ProfileDescription
 	},
-	beforeCreate() {
-		let userid = this.$route.params.id;
-		ApiHandler.getUser(userid).then(res => {
-			this.profileHeader.firstname = res.data.result.firstName;
-			this.profileHeader.lastname = res.data.result.lastName;
-			this.profileHeader.area = res.data.result.area;
-			this.profileHeader.picture = res.data.result.picture;
-			this.profileDesc = `Hej! Mitt namn är ${this.profileHeader.firstname}.
-				Jag bor i ${this.profileHeader.area} och vill gärna träffa nya människor
-				i mitt närområde.`;
-		});
-		ApiHandler.getDogs(userid).then(res => {
-			this.profileFamily = res.data.result;
-		});
-		ApiHandler.getPrefe(userid).then(res => {
-			if (res.data.result.description) {
-				this.profileDesc = res.data.result.description;
-			}
-		});
+	watch: {
+		//Set up a route watcher. Listens to changes in the route for a preexisting component.
+		//(e.g. routed from ProfilePage to ProfilePage)
+
+		//When components are reused through routers, lifecycle hooks are not called
+		$route(to) {
+			this.initializeUserData(to.params.id);
+		}
+	},
+	//Runs once when component is created.
+	//Does not run when routed from ProfilePage to ProfilePage, as the component is reused.
+	//E.g. when page is refreshed or when routed from another component to a ProfilePage
+	created() {
+		this.initializeUserData(this.$route.params.id);
 	},
 	data() {
 		return {
@@ -66,6 +61,28 @@ export default {
 			} else {
 				return "Föreslå promenad";
 			}
+		}
+	},
+	methods: {
+		//Retrieves user data and sets variables - rerendering child components who use them
+		initializeUserData(userid) {
+			ApiHandler.getUser(userid).then(res => {
+				this.profileHeader.firstname = res.data.result.firstName;
+				this.profileHeader.lastname = res.data.result.lastName;
+				this.profileHeader.area = res.data.result.area;
+				this.profileHeader.picture = res.data.result.picture;
+				this.profileDesc = `Hej! Mitt namn är ${this.profileHeader.firstname}.
+				Jag bor i ${this.profileHeader.area} och vill gärna träffa nya människor
+				i mitt närområde.`;
+			});
+			ApiHandler.getDogs(userid).then(res => {
+				this.profileFamily = res.data.result;
+			});
+			ApiHandler.getPrefe(userid).then(res => {
+				if (res.data.result.description) {
+					this.profileDesc = res.data.result.description;
+				}
+			});
 		}
 	}
 };
